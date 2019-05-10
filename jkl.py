078f0ace7068cfeadd265485d0f20f9045151bd7
yongqis <syq132@live.com>
2019-05-10 15:50:21 +0800

    ignore track jkl

--- jkl.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-import tensorflow as tf
-import numpy as np
-import os
-
-path = 'a/b/c/66.txt'
-label = os.path.split(os.path.dirname(path))
-print(label)
